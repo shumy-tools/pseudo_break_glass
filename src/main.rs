f2518c3455e67c5db96c64451798a2754469909d
shumy <micaelpedrosa@gmail.com>
2019-07-23 16:25:07 +0100

    Simulations for FedPI

--- src/main.rs
+++ src/main.rs
@@ -5,13 +5,15 @@ use std::time::{Instant, Duration};
 
 use clap::{Arg, App};
 
-use curve25519_dalek::ristretto::RistrettoPoint;
+use curve25519_dalek::ristretto::{RistrettoPoint, CompressedRistretto};
 use curve25519_dalek::scalar::Scalar;
 
-use sha2::Sha512;
+use sha2::{Sha512, Digest};
 use rand::prelude::*;
 
-use shamir::{Reconstruct, SecretSharing, RistrettoShare};
+use shamir::{Reconstruct, Polynomial, RistrettoPolynomial, Share, RistrettoShare};
+
+const RUNS: usize = 500;
 
 fn rand_scalar() -> Scalar {
     let val = rand::thread_rng().gen::<[u8; 32]>();
@@ -19,75 +21,189 @@ fn rand_scalar() -> Scalar {
 }
 
 #[allow(non_snake_case)]
-fn main() {
-    const RUNS: usize = 500;
+fn multiparty_stats(parties: usize, threshold: usize) {
+    println!("---Multiparty computation stats (500 runs)---");
+    let G: RistrettoPoint = RistrettoPoint::default();
 
-    let matches = App::new("Statistics for P-ID")
-        .version("1.0")
-        .author("Micael Pedrosa <micaelpedrosa@ua.pt>")
-        .about("Performs time measurements for runs of the PID function.")
-        .arg(Arg::with_name("parties")
-            .help("Sets the number of parties (n)")
-            .required(true)
-            .short("p")
-            .long("parties")
-            .takes_value(true))
-        .arg(Arg::with_name("threshold")
-            .help("Sets the threshold number (t)")
-            .required(true)
-            .short("t")
-            .long("threshold")
-            .takes_value(true))
-        .get_matches();
-
-    let str_parties = matches.value_of("parties").unwrap();
-    let str_threshold = matches.value_of("threshold").unwrap();
-
-    let parties = str_parties.parse::<usize>().unwrap();
-    let threshold = str_threshold.parse::<usize>().unwrap();
-    println!("Setup: (parties={}, threshold={})", parties, threshold);
-
-    let tss = SecretSharing { n: parties, t: threshold };
-
-    // setup the master key shares
     let y = rand_scalar();
-    let y_shares = tss.share(y);
+    let Y = y * G;
+
+    let poly = Polynomial::rnd(y, threshold);
+    let y_shares = poly.shares(parties);
+
+    // piece of public information
+    let public_x = rand::thread_rng().gen::<[u8; 32]>();
 
     let mut total = Duration::new(0, 0);
     for _ in 0..RUNS {
         let start = Instant::now();
 
-        // select a random fragment of public information and derive PC
-        let public_x = rand::thread_rng().gen::<[u8; 32]>();
+        // derive Pc from a fragment of public information
         let Pc = RistrettoPoint::hash_from_bytes::<Sha512>(&public_x);
         
-        // derive r * Pc => Rc
+        // derive Pc + R = W and Yr
         let r = rand_scalar();
-        let Rc = r * Pc;
+        let R = r * G;
+        let W = Pc + R;
+        let Yr = r * Y;
         
-        // client to parties transmission simulation (Rc)
+        // client to parties transmission simulation <W,Yr>
         //sleep(Duration::new(2, 0));
 
             // each party derives the partial pesudonym
-            let mut lambda_r_shares = y_shares.iter()
-                .map(|yi| yi * Rc).collect::<Vec<RistrettoShare>>();
+            let mut pi_shares = y_shares.iter().map(|yi| yi * W).collect::<Vec<RistrettoShare>>();
 
             // select a subset (> t) of random shares
             let mut rng = rand::thread_rng();
-            lambda_r_shares.shuffle(&mut rng);
-            let selected = &lambda_r_shares[0..threshold+1];
+            pi_shares.shuffle(&mut rng);
+            let selected = &pi_shares[0..threshold + 1];
 
-        // parties to client transmission simulation (selected)
+            // derive the pseudonym
+            let PI = Polynomial::reconstruct(selected, threshold) - Yr;
+
+        // party to client transmission simulation
         //sleep(Duration::new(2, 0));
 
-        let lambda_r = tss.reconstruct(selected);
-        let lambda = r.invert() * lambda_r;
-
         let run = Instant::now() - start;
-        assert!(y * Pc == lambda); // confirm the expected result of Lagrange interpolation
+        assert!(y * Pc == PI); // confirm the expected result
         total += run;
     }
 
     let avg = (total.as_micros() as f64) / (1000.0*RUNS as f64);
-    println!("Avg. per run ({:?} runs): {:?}ms", RUNS, avg);
+    println!("   Avg. per run: {:?}ms", avg);
+}
+
+#[allow(non_snake_case)]
+fn master_key_stats(parties: usize, threshold: usize) {
+    println!("---Master key setup stats---");
+    let G: RistrettoPoint = RistrettoPoint::default();
+
+    // setup all party keys
+    let mut p_si = Vec::<Scalar>::new();
+    let mut p_Pi = Vec::<RistrettoPoint>::new();
+    for _ in 0..parties {
+        let si = rand_scalar();
+        p_si.push(si);
+
+        let Pi = si * G;
+        p_Pi.push(Pi);
+    }
+
+    let mut total = Duration::new(0, 0);
+
+    // (Matrix Setup) setup the secret and public matrix
+    let start = Instant::now();
+
+    let mut s_matrix = Vec::<Vec::<Scalar>>::new();
+    let mut P_matrix = Vec::<Vec::<CompressedRistretto>>::new();
+    for i in 0..parties {
+        let mut i_p = Vec::<Scalar>::new();
+        let mut i_P = Vec::<CompressedRistretto>::new();
+        for j in 0..parties {
+            let dh_p = (p_si[i] * p_Pi[j]).compress(); // perform a Diffie-Hellman between parties (i, j)
+            let mut hasher = Sha512::new();
+            hasher.input(dh_p.as_bytes());
+            let dh_s = Scalar::from_hash(hasher);
+            i_p.push(dh_s);
+            i_P.push((dh_s * G).compress());
+        }
+
+        s_matrix.push(i_p);
+        P_matrix.push(i_P);
+    }
+
+    // is the public matrix symmetric?
+    for i in 0..parties {
+        for j in 0..parties {
+            assert!(P_matrix[i][j] == P_matrix[j][i]);
+        }
+    }
+
+    let run = Instant::now() - start;
+    total += run;
+    println!("   Matrix setup (size={:?}Kb, time={:?}ms)", ((P_matrix[0][0].as_bytes().len() * parties * parties) as f64) / 1024.0, (run.as_micros() as f64) / 1000.0);
+    
+    // (Step 1 & 2) commit to pre-defined polynomials and construct encrypted shares
+    let start = Instant::now();
+    
+    let mut y = Scalar::zero();
+    let mut i_Ak = Vec::<RistrettoPolynomial>::new();
+    let mut i_e_j = Vec::<Vec::<Share>>::new();
+    for i in 0..parties {
+        let i_y = rand_scalar();
+        y += i_y;
+
+        let i_ak = Polynomial::rnd(i_y, threshold);
+        let i_y_shares = i_ak.shares(parties);
+
+        // commit to a polynomial
+        i_Ak.push(i_ak * G);
+
+        // encrypt shares
+        let mut e_j = Vec::<Share>::new();
+        for j in 0..parties {
+            e_j.push( s_matrix[i][j] + i_y_shares[j] );
+        }
+        i_e_j.push(e_j);
+    }
+
+    let run = Instant::now() - start;
+    total += run;
+    println!("   Commit and encrypt: {:?}ms", (run.as_micros() as f64) / 1000.0);
+
+    // (Step 3) verify if shares are correct
+    let start = Instant::now();
+
+    for i in 0..parties {
+        for j in 0..parties {
+            let i_Y_j = i_e_j[i][j] * G - P_matrix[i][j].decompress().unwrap();
+            assert!(i_Ak[i].verify(i_Y_j) == true);
+        }
+    }
+
+    let run = Instant::now() - start;
+    total += run;
+    println!("   Share verification: {:?}ms", (run.as_micros() as f64) / 1000.0);
+
+    // reconstruct master key
+    let start = Instant::now();
+
+    let mut y_j = Vec::<Share>::new();
+    for j in 0..parties {
+        // sum the shares of different sources
+        let mut sum_j = Scalar::zero();
+        for i in 0..parties {
+            sum_j += i_e_j[i][j].yi - s_matrix[i][j];
+        }
+        y_j.push(Share { i: i_e_j[0][j].i, yi: sum_j });
+    }
+    let y_res = Polynomial::reconstruct(&y_j, threshold);
+    assert!(y == y_res);
+
+    let run = Instant::now() - start;
+    total += run;
+    println!("   Total: {:?}ms", (total.as_micros() as f64) / 1000.0);
+}
+
+fn main() {
+    let matches = App::new("Statistics for FedPI")
+        .version("1.0")
+        .author("Micael Pedrosa <micaelpedrosa@ua.pt>")
+        .about("Simulations and measurements for the FedPI protocol.")
+        .arg(Arg::with_name("threshold")
+            .help("Sets the threshold number (t). The number of parties are set automatically to 3t+1.")
+            .required(true)
+            .short("t")
+            .long("threshold")
+            .takes_value(true))
+        .get_matches();
+    
+    let str_threshold = matches.value_of("threshold").unwrap();
+
+    let threshold = str_threshold.parse::<usize>().unwrap();
+    let parties = 3 * threshold + 1;
+    println!("Setup: (t={}, 3t+1={})", threshold, parties);
+
+    master_key_stats(parties, threshold);
+    multiparty_stats(parties, threshold);
 }
--- src/shamir.rs
+++ src/shamir.rs
@@ -5,23 +5,10 @@ use rand_os::OsRng;
 
 use core::ops::{Add, Mul, Sub};
 
-fn base_coeficient<F>(size: usize, i: usize, xi: u32, get_xj: F) -> Scalar
-    where F: Fn(usize) -> u32 {
-    
-    let sxi = Scalar::from(xi);
-    let mut num = Scalar::one();
-    let mut denum = Scalar::one();
-    for j in 0..size {
-        if j != i {
-            let xj = Scalar::from(get_xj(j));
-            num *= xj;
-            denum *= xj - sxi;
-        }
-    }
 
-    num * denum.invert()
-}
-
+//-----------------------------------------------------------------------------------------------------------
+// Share
+//-----------------------------------------------------------------------------------------------------------
 #[derive(Copy, Clone)]
 pub struct Share {
     pub i: u32,
@@ -86,11 +73,35 @@ impl<'a, 'b> Mul<&'b RistrettoPoint> for &'a Share {
     }
 }
 
+
+//-----------------------------------------------------------------------------------------------------------
+// RistrettoShare
+//-----------------------------------------------------------------------------------------------------------
 #[allow(non_snake_case)]
 #[derive(Copy, Clone)]
 pub struct RistrettoShare {
     pub i: u32,
     pub Yi: RistrettoPoint
+}
+
+define_add_variants!(LHS = RistrettoShare, RHS = RistrettoPoint, Output = RistrettoShare);
+define_add_variants!(LHS = RistrettoPoint, RHS = RistrettoShare, Output = RistrettoShare);
+define_comut_add!(LHS = RistrettoPoint, RHS = RistrettoShare, Output = RistrettoShare);
+impl<'a, 'b> Add<&'b RistrettoPoint> for &'a RistrettoShare {
+    type Output = RistrettoShare;
+    fn add(self, rhs: &'b RistrettoPoint) -> RistrettoShare {
+        RistrettoShare { i: self.i, Yi: self.Yi + rhs }
+    }
+}
+
+define_sub_variants!(LHS = RistrettoShare, RHS = RistrettoPoint, Output = RistrettoShare);
+define_sub_variants!(LHS = RistrettoPoint, RHS = RistrettoShare, Output = RistrettoShare);
+define_comut_sub!(LHS = RistrettoPoint, RHS = RistrettoShare, Output = RistrettoShare);
+impl<'a, 'b> Sub<&'b RistrettoPoint> for &'a RistrettoShare {
+    type Output = RistrettoShare;
+    fn sub(self, rhs: &'b RistrettoPoint) -> RistrettoShare {
+        RistrettoShare { i: self.i, Yi: self.Yi - rhs }
+    }
 }
 
 define_mul_variants!(LHS = RistrettoShare, RHS = Scalar, Output = RistrettoShare);
@@ -103,51 +114,128 @@ impl<'a, 'b> Mul<&'b Scalar> for &'a RistrettoShare {
     }
 }
 
+
+//-----------------------------------------------------------------------------------------------------------
+// Shared traits and functions for Polynomial and RistrettoPolynomial
+//-----------------------------------------------------------------------------------------------------------
+fn base_coeficient<F>(size: usize, i: usize, xi: u32, get_xj: F) -> Scalar
+    where F: Fn(usize) -> u32 {
+    
+    let sxi = Scalar::from(xi);
+    let mut num = Scalar::one();
+    let mut denum = Scalar::one();
+    for j in 0..size {
+        if j != i {
+            let xj = Scalar::from(get_xj(j));
+            num *= xj;
+            denum *= xj - sxi;
+        }
+    }
+
+    num * denum.invert()
+}
+
 pub trait Reconstruct<S> {
     type Output;
-    fn reconstruct(&self, shares: &[S]) -> Self::Output;
+    fn reconstruct(shares: &[S], k: usize) -> Self::Output;
 }
 
-pub struct SecretSharing {
-    pub n: usize,
-    pub t: usize
+pub trait Evaluate {
+    type Output;
+    fn evaluate(&self, x: Scalar) -> Self::Output;
 }
 
-impl SecretSharing {
-    pub fn share(&self, secret: Scalar) -> Vec<Share> {
-        let poly = self.sample_polynomial(secret);
-        self.evaluate_polynomial(&poly).iter().enumerate()
-            .map(|(n, y)| Share{ i: (n + 1) as u32, yi: *y }).collect()
+
+//-----------------------------------------------------------------------------------------------------------
+// RistrettoPolynomial
+//-----------------------------------------------------------------------------------------------------------
+#[allow(non_snake_case)]
+pub struct RistrettoPolynomial {
+    pub k: usize,
+    pub A: Vec<RistrettoPoint>
+}
+
+define_mul_variants!(LHS = Polynomial, RHS = RistrettoPoint, Output = RistrettoPolynomial);
+define_mul_variants!(LHS = RistrettoPoint, RHS = Polynomial, Output = RistrettoPolynomial);
+define_comut_mul!(LHS = RistrettoPoint, RHS = Polynomial, Output = RistrettoPolynomial);
+impl<'a, 'b> Mul<&'b RistrettoPoint> for &'a Polynomial {
+    type Output = RistrettoPolynomial;
+    fn mul(self, rhs: &'b RistrettoPoint) -> RistrettoPolynomial {
+        RistrettoPolynomial {
+            k: self.k,
+            A: self.a.iter().map(|ak| ak * rhs).collect::<Vec<RistrettoPoint>>()
+        }
     }
+}
 
-    fn sample_polynomial(&self, zero_value: Scalar) -> Vec<Scalar> {
-        let mut coefficients = vec![zero_value];
+impl RistrettoPolynomial {
+    pub fn verify(&self, share: RistrettoShare) -> bool {
+        let x = Scalar::from(share.i as u64);
+        share.Yi == self.evaluate(x)
+    }
+}
+
+impl Evaluate for RistrettoPolynomial {
+    type Output = RistrettoPoint;
+    
+    fn evaluate(&self, x: Scalar) -> RistrettoPoint {
+        // evaluate using Horner's rule
+        let mut rev = self.A.iter().rev();
+        let head = *rev.next().unwrap();
+            
+        rev.fold(head, |partial, coef| partial * x + coef)
+    }
+}
+
+
+//-----------------------------------------------------------------------------------------------------------
+// Polynomial
+//-----------------------------------------------------------------------------------------------------------
+pub struct Polynomial {
+    pub k: usize,
+    pub a: Vec<Scalar>
+}
+
+impl Polynomial {
+    pub fn rnd(secret: Scalar, degree: usize) -> Self {
+        let mut coefs = vec![secret];
 
         let mut csprng: OsRng = OsRng::new().unwrap();
-        let random_coefficients: Vec<Scalar> = (0..self.t).map(|_| Scalar::random(&mut csprng)).collect();
-
-        coefficients.extend(random_coefficients);
-        coefficients
+        let rnd_coefs: Vec<Scalar> = (0..degree).map(|_| Scalar::random(&mut csprng)).collect();
+        coefs.extend(rnd_coefs);
+        
+        Polynomial { k: degree, a: coefs }
     }
 
-    fn evaluate_polynomial(&self, coefficients: &[Scalar]) -> Vec<Scalar> {
-        (1..self.n + 1).map(|x| {
-            let point = Scalar::from(x as u64);
+    pub fn shares(&self, n: usize) -> Vec<Share> {
+        let mut shares = Vec::<Share>::new();
+        for j in 1..n + 1 {
+            let x = Scalar::from(j as u64);
+            let share = Share { i: j as u32, yi: self.evaluate(x) };
+            shares.push(share);
+        }
 
-            // evaluate using Horner's rule
-            let mut rev = coefficients.iter().rev();
-            let head = *rev.next().unwrap();
-            
-            rev.fold(head, |partial, coef| partial * point + coef)
-        }).collect()
+        shares
     }
 }
 
-impl Reconstruct<Share> for SecretSharing {
+impl Evaluate for Polynomial {
     type Output = Scalar;
     
-    fn reconstruct(&self, shares: &[Share]) -> Scalar {
-        assert!(shares.len() >= self.t + 1);
+    fn evaluate(&self, x: Scalar) -> Scalar {
+        // evaluate using Horner's rule
+        let mut rev = self.a.iter().rev();
+        let head = *rev.next().unwrap();
+            
+        rev.fold(head, |partial, coef| partial * x + coef)
+    }
+}
+
+impl Reconstruct<Share> for Polynomial {
+    type Output = Scalar;
+    
+    fn reconstruct(shares: &[Share], k: usize) -> Scalar {
+        assert!(shares.len() >= k + 1);
         let size = shares.len();
 
         let mut acc = Scalar::zero();
@@ -162,12 +250,12 @@ impl Reconstruct<Share> for SecretSharing {
     }
 }
 
-impl Reconstruct<RistrettoShare> for SecretSharing {
+impl Reconstruct<RistrettoShare> for Polynomial {
     type Output = RistrettoPoint;
 
     #[allow(non_snake_case)]
-    fn reconstruct(&self, shares: &[RistrettoShare]) -> RistrettoPoint {
-        assert!(shares.len() >= self.t + 1);
+    fn reconstruct(shares: &[RistrettoShare], k: usize) -> RistrettoPoint {
+        assert!(shares.len() >= k + 1);
         let size = shares.len();
 
         let mut res = Vec::<RistrettoPoint>::with_capacity(size);
